93ccd108e6652d6a5965454fd43e9e35eb36d41e
Mazunki Hoksaas <rolferen@gmail.com>
2026-04-29 10:06:55 +0200

    implement munmap properly

--- src/musl/munmap.cpp
+++ src/musl/munmap.cpp
@@ -1,14 +1,39 @@
 #include "common.hpp"
+#include <util/pretty.hpp>
+#include <kernel/memory.hpp>
 
-extern "C" void kfree(void* addr, size_t length);
+// #define TRACE_MUNMAP
+#if defined(TRACE_MUNMAP)
+  #define MUNMAP_TRACE(fmt, ...) std::println("\033[0;94m{}\033[0m: \033[0;90m" fmt "\033[0m", __FUNCTION__, ##__VA_ARGS__)
+#else
+  #define MUNMAP_TRACE(...) do {} while(0)
+#endif
 
-static long sys_munmap(void *addr, size_t length)
+
+os::mem::mem_resource& default_allocator = os::mem::raw_allocator();
+
+static long munmap_failed(int err) {
+  errno = err;
+  return err;
+}
+
+
+static long __sys_munmap(uintptr_t addr, size_t length)
 {
   if(UNLIKELY(length == 0))
-    return -EINVAL;
+    return munmap_failed(EINVAL);
 
-  kfree(addr, length);
+  MUNMAP_TRACE("({}, {}) ", KVF(addr, "{:#x}"), KVA(length, util::format::Bytes{length}));
+
+  default_allocator.deallocate(reinterpret_cast<void*>(addr), length);
   return 0;
+}
+
+static long sys_munmap(void* _addr, size_t length)
+{
+  const uintptr_t addr = reinterpret_cast<uintptr_t>(_addr);
+
+  return __sys_munmap(addr, length);
 }
 
 extern "C"
